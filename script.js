6325af371efbe0a82a639c4f608bcd71641f839b
manu0619 <125184498+manu0619@users.noreply.github.com>
2025-01-09 02:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// let counter = 1; // Initialize a global counter
-
 function insert_Row() {
   // Get the table element by its ID
   let table = document.querySelector("#sampleTable");
 
   // Add a new row with incremented cell values using innerHTML
   table.innerHTML =
-    `<tr><td>New Cell1</td><td>New Cell2</td></tr>` +
-    table.innerHTML;
+    `<tr><td>New Cell1</td><td>New Cell2</td></tr>` + table.innerHTML;
 
   // Increment the counter by 2 for the next row
-  counter += 2;
+  //   counter += 2;
 }
